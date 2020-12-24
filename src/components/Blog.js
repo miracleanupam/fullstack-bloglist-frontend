1193ba3994c3aef02b0cd60ad01c2cc2eb664ea4
Anupam Dahal <miracleanupam@gmail.com>
2020-12-24 23:45:53 +0200

    exercise 5.7; button to toggle expansion of blog post and some styles

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,24 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  return (
+    <div className='blogPost'>
+      {blog.title} {blog.author} <button type='button' onClick={toggleExpanded}>{isExpanded ? 'hide' : 'view'}</button>
+      { isExpanded ? (
+        <div>
+          <p>{blog.url}</p>
+          <p>{blog.likes}</p>
+          <p>{blog.user.name}</p>
+        </div>
+        ) : (<div></div>)}
+    </div>
+  );
+};
+
+export default Blog;
