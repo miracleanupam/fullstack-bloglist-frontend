0405c3d2ebbc51fb47155664bc6c6753f8376ac8
Anupam Dahal <miracleanupam@gmail.com>
2020-12-25 12:20:51 +0200

    exercise 5.14; test that blogs urls and other info is shown when view button is clicked

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
 import Blog from './Blog';
 
 
@@ -9,7 +9,10 @@ test('Blog component renders only title and author by default', () => {
         title: 'Blog Component',
         author: 'Test User',
         likes: 5,
-        url: 'www.google.com'
+        url: 'www.google.com',
+        user: {
+            name: 'Tester'
+        }
     }
 
     const component = render(
@@ -24,3 +27,27 @@ test('Blog component renders only title and author by default', () => {
         'www.google.com'
     );
 });
+
+
+test('Blog\'s url and likes are shown when view button is clicked', () => {
+    const blog = {
+        title: 'Blog Component',
+        author: 'Test User',
+        likes: 5,
+        url: 'www.google.com',
+        user: {
+            name: 'Tester'
+        }
+    };
+
+    const mockHandler = jest.fn();
+
+    const component = render(
+        <Blog blog={blog} />
+    );
+    const button = component.getByText('view');
+    fireEvent.click(button);
+
+    expect(component.container).toHaveTextContent('5');
+    expect(component.container).toHaveTextContent('www.google.com');
+})
